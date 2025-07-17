6d30c5db6b61eba8706f447017527a91e99bb8e9
GChimel <gustavochimel12@gmail.com>
2025-07-17 16:49:49 -0300

    fix: Adding optional to update-task.dto.ts

--- src/modules/task/dto/update-task.dto.ts
+++ src/modules/task/dto/update-task.dto.ts
@@ -1,9 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from 'src/shared/database/entities/task.entity';
 import { CreateTaskDto } from './create-task.dto';
 
-export class UpdateTaskDto extends CreateTaskDto {
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @ApiProperty({ example: TaskStatus.PENDING, enum: TaskStatus })
   @IsNotEmpty()
   @IsEnum(TaskStatus)
